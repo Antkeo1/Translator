5c9561ea3e6d3419d3b466ba3c3be411b5c445e4
Antkeo1 <sleav143@gmail.com>
2019-04-30 19:04:58 -0400

    initial commit

---
+++ src/Components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class App extends React.Component {
+  state = {
+
+  }
+
+  render() {
+    return (
+      <div className='ui container '>
+        App
+      </div>
+    )
+  }
+}
+
+export default App
