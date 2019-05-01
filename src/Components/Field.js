e9eaf88b3895de78e33ad28f87cbc579f6776186
Antkeo1 <sleav143@gmail.com>
2019-05-01 09:41:01 -0400

    created UserCreate, Field and button component

---
+++ src/Components/Field.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Field extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className='ui field'>
+          <label>Name</label>
+          <input />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Field
